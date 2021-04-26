63c25d208354c10e1e6e84c1ba6cc99175f2df5a
lxndrfgrd <lxndrfgrd@outlook.com>
2021-04-26 12:08:39 +0200

    Update upgrade.sh

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -9,16 +9,16 @@ cat ../src/info.txt
 echo "[*] Koala Toolkit Upgrade"
 
 echo "[*] Uninstall current version"
-docker container kill thelittlebigbot/koala
-docker container rm thelittlebigbot/koala
-docker image rm thelittlebigbot/koala
+docker container kill lxndrfgrd/koala
+docker container rm lxndrfgrd/koala
+docker image rm lxndrfgrd/koala
 docker image rm kalilinux/kali-rolling
 
 rm -rf $BIN
 rm -rf $DIR
 
-echo "[*] Git clone thelittlebigbot/koala"
-git clone https://github.com/thelittlebigbot/koala.git $DIR
+echo "[*] Git clone lxndrfgrd/koala"
+git clone https://github.com/lxndrfgrd/koala.git $DIR
 
 echo "[*] Install scripts in /usr/local/bin"
 chmod +x $KOALA && ln -sf $KOALA $BIN
