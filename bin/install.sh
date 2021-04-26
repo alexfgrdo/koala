07c6f47c5b6148fc9db37c357b221b76e2448574
lxndrfgrd <lxndrfgrd@outlook.com>
2021-04-26 12:07:44 +0200

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -9,10 +9,10 @@ cat ../src/info.txt
 echo "[*] Koala Toolkit Installer"
 
 echo "[*] Git clone theilttlebigbot/koala"
-git clone https://github.com/thelittlebigbot/koala.git $DIR
+git clone https://github.com/lxndrfgrd/koala.git $DIR
 
 echo "[*] Install scripts in /usr/local/bin/"
 chmod +x $KOALA && ln -sf $KOALA $BIN
 
 echo "[*] Build"
-docker build --rm --no-cache -t thelittlebigbot/koala $DIR/
+docker build --rm --no-cache -t lxndrfgrd/koala $DIR/
