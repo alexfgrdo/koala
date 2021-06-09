acca585f0ab6d707fd54297198c72e70b1f91e20
Alexandre Figueiredo <afigueir@protonmail.com>
2021-06-09 18:08:25 +0200

    Update upgrade.sh

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -9,16 +9,16 @@ cat ../src/info.txt
 echo "[*] Koala Toolkit Upgrade"
 
 echo "[*] Uninstall current version"
-docker container kill lxndrfgrd/koala
-docker container rm lxndrfgrd/koala
-docker image rm lxndrfgrd/koala
+docker container kill afigueir/koala
+docker container rm afigueir/koala
+docker image rm afigueir/koala
 docker image rm kalilinux/kali-rolling
 
 rm -rf $BIN
 rm -rf $DIR
 
 echo "[*] Git clone lxndrfgrd/koala"
-git clone https://github.com/lxndrfgrd/koala.git $DIR
+git clone https://github.com/afigueir/koala.git $DIR
 
 echo "[*] Install scripts in /usr/local/bin"
 chmod +x $KOALA && ln -sf $KOALA $BIN
