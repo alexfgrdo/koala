a9c84c45f9e89e2b1df2a333007256b42967ffb1
Alexandre Figueiredo <afigueir@protonmail.com>
2021-06-09 18:07:11 +0200

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -8,11 +8,11 @@ cat ../src/info.txt
 
 echo "[*] Koala Toolkit Installer"
 
-echo "[*] Git clone theilttlebigbot/koala"
-git clone https://github.com/lxndrfgrd/koala.git $DIR
+echo "[*] Git clone afigueir/koala"
+git clone https://github.com/afigueir/koala.git $DIR
 
 echo "[*] Install scripts in /usr/local/bin/"
 chmod +x $KOALA && ln -sf $KOALA $BIN
 
 echo "[*] Build"
-docker build --rm --no-cache -t lxndrfgrd/koala $DIR/
+docker build --rm --no-cache -t afigueir/koala $DIR/
